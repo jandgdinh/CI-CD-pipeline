cee9474a8a58df8543f7a04d4fdd548ec5a7f980
John <jandgdinh@gmail.com>
2024-11-14 19:12:42 -0800

    added cleandb changes

--- server/dist/seeds/cleanDb.js
+++ server/dist/seeds/cleanDb.js
@@ -2,7 +2,11 @@ import models from '../models/index.js';
 import db from '../config/connection.js';
 export default async (modelName, collectionName) => {
     try {
-        let modelExists = await models[modelName].db.db.listCollections({
+        const model = models[modelName];
+        if (!model || !model.db || !model.db.db) {
+            throw new Error(`Model or database connection is not defined for model: ${modelName}`);
+        }
+        const modelExists = await model.db.db.listCollections({
             name: collectionName
         }).toArray();
         if (modelExists.length) {
